6327b2757445c70369acfd1238d246156a7511fc
Diogo Mendes <ogoid2000@gmail.com>
2025-05-09 17:35:45 +0100

    Add velocity attribute and acceleration method to Carros class

--- Aula_7/Classes.Carros.py
+++ Aula_7/Classes.Carros.py
@@ -1,8 +1,11 @@
 
 class Carros:
-    def __init__ (self,modelo,marca,ano):
+    def __init__ (self,modelo,marca,ano,velocidade):
         self.modelo = modelo
         self.marca = marca
         self.ano = ano
         self.velocidade = velocidade
-        
+
+    def Acelera(self, nivel):
+        self.velocidade += nivel
+        print(f"A velocidade atual é {self.velocidade} km/h")
